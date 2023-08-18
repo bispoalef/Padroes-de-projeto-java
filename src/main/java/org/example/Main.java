0ef41aca3315607058f290bbde7fe09e686f312c
bispoalef <alef.b.dos.santos.1993@gmail.com>
2023-08-18 20:18:44 -0300

    # criando classes e testando os valores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,17 +10,26 @@ public class Main {
 
 
         ContaCorrente conta1 = new ContaCorrente("01", "12345", 1000.0);
+        ContaCorrente conta2 = new ContaCorrente("02", "67890");
+
+        ContaPoupanca contaPoupanca1 = new ContaPoupanca("001", "101112", 1, 10);
+        ContaPoupanca contaPoupanca2 = new ContaPoupanca("002", "131415", 2, 20);
+
+        conta1.setTitular(new Titular("Alef", "123.456.789-00"));
+        conta2.setTitular(new Titular("Fausto", "987.654.321-00"));
+
+        contaPoupanca1.setTitular(new Titular("Viviane", "555.555.555-55"));
+        contaPoupanca2.setTitular(new Titular("Everton", "777.777.777-77"));
 
         conta1.depositar(15000.0);
         conta1.sacar(200.0);
 
-        conta1.setTitular(new Titular("Alef", "123.456.789-00"));
-
-        ContaPoupanca contaPoupanca1 = new ContaPoupanca("001", "101112", 1, 10);
-
-        contaPoupanca1.setTitular(new Titular("Viviane", "555.555.555-55"));
+        contaPoupanca2.depositar(3600.0);
+        contaPoupanca2.sacar(300.0);
 
         System.out.println(conta1);
+        System.out.println(conta2);
         System.out.println(contaPoupanca1);
+        System.out.println(contaPoupanca2);
     }
 }
