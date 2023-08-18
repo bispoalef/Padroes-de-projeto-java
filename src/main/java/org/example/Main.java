552916b004e0f80d6ba076b54c4ee7412219d61c
bispoalef <alef.b.dos.santos.1993@gmail.com>
2023-08-18 19:58:38 -0300

    Criando projeto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.banco.model.Conta;
+import org.example.banco.model.ContaCorrente;
+import org.example.banco.model.ContaPoupanca;
+import org.example.banco.model.Titular;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
